35eab5a8b50b2082cca592fb60de804b914a6516
JOEL P BASIL <94457069+Ha-p-p-y@users.noreply.github.com>
2024-09-07 17:22:42 +0530

    round off.py
    rounding off any input number into the bases of 10's and 100's according to user input

--- round off.py
+++ round off.py
@@ -1,9 +1,20 @@
+z=eval(input("ENTE '1'FOR IN TERMS OF 10 & AND '2' FOR IN TERMS OF 100 "))
 a=eval(input('ENTER THE ROUND OFFABLE NUMBER = '))
-b=a%10
-c=10-b
-if b>=5 :
-    d=a-c
-    print('THE NUMBER IS = ',d)
+if z==1:
+    b=a%10
+    c=10-b
+    if b>=5 :
+        d=a+c
+        print('THE NUMBER IS = ',d)
+    else :
+        e=a-b
+        print('THE NUMBER IS = ',e)
 else :
-    e=a+c
-    print('THE NUMBER IS = ',e)
+        b=a%100
+        c=100-b
+        if b>=50 :
+            d=a+c
+            print('THE NUMBER IS = ',d)
+        else :
+            e=a-b
+            print('THE NUMBER IS = ',e)
